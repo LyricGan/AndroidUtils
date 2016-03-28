c6cdafef7c480d0f2d8c140abc6f0fbe199eb249
lyricgan <lyricgan88@163.com>
2016-03-28 17:00:21 +0800

    update

---
+++ library/src/main/java/com/lyric/android/library/utils/PreferencesUtils.java
@@ -0,0 +1,112 @@
+package com.lyric.android.library.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * @author lyric
+ * @description
+ * @time 2016/03/21 14:36
+ */
+public class PreferencesUtils {
+    public static final String PREFERENCE_NAME = "Android_Utils";
+
+    private PreferencesUtils() {
+        throw new AssertionError();
+    }
+
+    public static void putString(Context context, String key, String value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String key) {
+        return getString(context, key, "");
+    }
+
+    public static String getString(Context context, String key, String defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getString(key, defaultValue);
+    }
+
+    public static void putInt(Context context, String key, int value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    public static int getInt(Context context, String key) {
+        return getInt(context, key, 0);
+    }
+
+    public static int getInt(Context context, String key, int defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getInt(key, defaultValue);
+    }
+
+    public static void putLong(Context context, String key, long value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(key, value);
+        editor.apply();
+    }
+
+    public static long getLong(Context context, String key) {
+        return getLong(context, key, -1);
+    }
+
+    public static long getLong(Context context, String key, long defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getLong(key, defaultValue);
+    }
+
+    public static void putFloat(Context context, String key, float value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putFloat(key, value);
+        editor.apply();
+    }
+
+    public static float getFloat(Context context, String key) {
+        return getFloat(context, key, -1);
+    }
+
+    public static float getFloat(Context context, String key, float defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getFloat(key, defaultValue);
+    }
+
+    public static void putBoolean(Context context, String key, boolean value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public static boolean getBoolean(Context context, String key) {
+        return getBoolean(context, key, false);
+    }
+
+    public static boolean getBoolean(Context context, String key, boolean defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return settings.getBoolean(key, defaultValue);
+    }
+
+    public static void clear(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+    public static void remove(Context context, String key) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove(key);
+        editor.apply();
+    }
+
+}
