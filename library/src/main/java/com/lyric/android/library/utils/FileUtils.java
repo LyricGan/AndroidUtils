7c97ecd7900b2ec9cdbad11d18ed0797d4cf1820
lyricgan <lyricgan88@163.com>
2016-06-08 18:14:54 +0800

    update

--- library/src/main/java/com/lyric/android/library/utils/FileUtils.java
+++ library/src/main/java/com/lyric/android/library/utils/FileUtils.java
@@ -7,7 +7,7 @@ import java.io.File;
 
 /**
  * 文件工具类
- * 
+ *
  * @author ganyu
  * @version 2015-9-14
  */
@@ -15,24 +15,24 @@ public class FileUtils {
 
     private FileUtils() {
     }
-	
-	/**
-	 * 删除缓存文件
-	 * @param context Context
-	 */
-	public static void deleteCacheFolder(Context context) {
-		clearCacheFolder(context.getCacheDir(), System.currentTimeMillis());
-	}
-	
-	/**
-	 * 删除缓存文件
-	 * @param dir 文件目录
-	 * @param lastModified 时间戳
-	 * @return 被删除文件数量
-	 */
-	public static int clearCacheFolder(File dir, long lastModified) {
-		int deletedFileCount = 0;
-		if (dir != null && dir.isDirectory()) {
+
+    /**
+     * 删除缓存文件
+     * @param context Context
+     */
+    public static void deleteCacheFolder(Context context) {
+        clearCacheFolder(context.getCacheDir(), System.currentTimeMillis());
+    }
+
+    /**
+     * 删除缓存文件
+     * @param dir          文件目录
+     * @param lastModified 时间戳
+     * @return 被删除文件数量
+     */
+    public static int clearCacheFolder(File dir, long lastModified) {
+        int deletedFileCount = 0;
+        if (dir != null && dir.isDirectory()) {
             for (File file : dir.listFiles()) {
                 if (file.isDirectory()) {
                     deletedFileCount += clearCacheFolder(file, lastModified);
@@ -43,16 +43,16 @@ public class FileUtils {
                     }
                 }
             }
-		}
-		return deletedFileCount;
-	}
+        }
+        return deletedFileCount;
+    }
 
     /**
      * 判断文件是否存在
      * @param filePath 文件路径
      * @return boolean
      */
-    public static boolean isFileExists(String filePath) {
+    public static boolean isExists(String filePath) {
         if (!TextUtils.isEmpty(filePath)) {
             File file = new File(filePath);
             if (file.exists()) {
