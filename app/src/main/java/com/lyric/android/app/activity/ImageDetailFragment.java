ecd5d8e26719ea4d4534b7f8338b7a205a7fa950
lyricgan <lyricgan88@163.com>
2016-06-23 15:56:24 +0800

    update

--- app/src/main/java/com/lyric/android/app/activity/ImageDetailFragment.java
+++ app/src/main/java/com/lyric/android/app/activity/ImageDetailFragment.java
@@ -1,22 +1,26 @@
 package com.lyric.android.app.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.lyric.android.app.R;
 import com.lyric.android.app.widget.photoview.PhotoViewAttacher;
-import com.lyric.android.library.image.NetworkImageView;
+import com.lyric.android.library.utils.LogUtils;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 
 public class ImageDetailFragment extends Fragment {
 	private static final String TAG = ImageDetailFragment.class.getSimpleName();
 	/** 图片地址 */
 	private String mImageUrl;
-	private NetworkImageView mImageView;
+	private ImageView mImageView;
 	private ProgressBar mProgressBar;
 	private PhotoViewAttacher mAttacher;
 
@@ -38,7 +42,7 @@ public class ImageDetailFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.fragment_image_detail, container, false);
-		mImageView = (NetworkImageView) view.findViewById(R.id.iv_picture);
+		mImageView = (ImageView) view.findViewById(R.id.iv_picture);
 		mAttacher = new PhotoViewAttacher(mImageView);
 		mAttacher.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
 			
@@ -54,16 +58,44 @@ public class ImageDetailFragment extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-        mProgressBar.setVisibility(View.VISIBLE);
-        mImageView.setVisibility(View.GONE);
-        mImageView.setImageUrl(mImageUrl);
-        new Handler().postDelayed(new Runnable() {
+        ImageLoader.getInstance().displayImage(mImageUrl, mImageView, new SimpleImageLoadingListener() {
+
             @Override
-            public void run() {
+            public void onLoadingStarted(String imageUri, View view) {
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                String message = null;
+                switch (failReason.getType()) {
+                    case IO_ERROR:
+                        message = "下载错误";
+                        break;
+                    case DECODING_ERROR:
+                        message = "图片无法显示";
+                        break;
+                    case NETWORK_DENIED:
+                        message = "网络有问题，无法下载";
+                        break;
+                    case OUT_OF_MEMORY:
+                        message = "图片太大无法显示";
+                        break;
+                    case UNKNOWN:
+                        message = "未知的错误";
+                        break;
+                }
+                LogUtils.e(TAG, "message:" + message);
+
                 mProgressBar.setVisibility(View.GONE);
-                mImageView.setVisibility(View.VISIBLE);
                 mAttacher.update();
             }
-        }, 3000);
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                mProgressBar.setVisibility(View.GONE);
+                mAttacher.update();
+            }
+        });
 	}
 }
--- app/src/main/java/com/lyric/android/app/base/BaseApplication.java
+++ app/src/main/java/com/lyric/android/app/base/BaseApplication.java
@@ -1,9 +1,21 @@
 package com.lyric.android.app.base;
 
 import android.app.Application;
+import android.graphics.Bitmap;
 
+import com.lyric.android.app.R;
 import com.lyric.android.app.constants.Constants;
 import com.lyric.android.library.utils.LogUtils;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
+import java.io.File;
 
 /**
  * extends Application {@link Application}
@@ -27,6 +39,8 @@ public class BaseApplication extends Application {
 //        } else {
 //            mRefWatcher = RefWatcher.DISABLED;
 //        }
+
+        initImageLoader();
 	}
 
 	public static BaseApplication getContext() {
@@ -36,4 +50,32 @@ public class BaseApplication extends Application {
 //    public static RefWatcher getRefWatcher() {
 //        return mRefWatcher;
 //    }
+
+    private void initImageLoader() {
+        // 初始化图片加载类
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageForEmptyUri(R.mipmap.merch_cover_default)
+                .showImageOnLoading(R.mipmap.merch_cover_default)
+                .showImageOnFail(R.mipmap.merch_cover_default)
+                .cacheInMemory(true)
+                .cacheOnDisc(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+
+        // 设置图片文件缓存路径
+        String imageDirectory = "/Android/data/" + getApplicationContext().getPackageName() + "/cache/image_loader_cache";
+        File cacheDir = StorageUtils.getOwnCacheDirectory(getApplicationContext(), imageDirectory);
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .threadPoolSize(3)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .memoryCache(new WeakMemoryCache())
+                .discCache(new UnlimitedDiscCache(cacheDir))
+                .defaultDisplayImageOptions(defaultOptions)
+//			.writeDebugLogs()
+                .build();
+        ImageLoader.getInstance().init(config);
+    }
 }
