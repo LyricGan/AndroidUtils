272221d529e612f637ffddf214d1d11367aff753
lyricgan <lyricgan88@163.com>
2016-06-24 19:20:26 +0800

    update

--- library/src/main/java/com/lyric/android/library/utils/PermissionUtils.java
+++ library/src/main/java/com/lyric/android/library/utils/PermissionUtils.java
@@ -3,6 +3,7 @@ package com.lyric.android.library.utils;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 
 /**
  * @author 应用权限控制工具类
@@ -21,8 +22,29 @@ public class PermissionUtils {
     private PermissionUtils() {
     }
 
+    /**
+     * 判断权限是否被拒绝
+     * @param context Context
+     * @param permission 应用权限
+     * @return returns true if permission is denied
+     */
     public static boolean check(Context context, String permission) {
-        int result = context.checkCallingOrSelfPermission(permission);
-        return (PackageManager.PERMISSION_GRANTED == result);
+        return (PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(context, permission));
+    }
+
+    /**
+     * 判断权限是否全部被拒绝
+     * @param context Context
+     * @param permissions 应用权限
+     * @return returns true if permissions is denied
+     */
+    public static boolean check(Context context, String... permissions) {
+        CheckUtils.checkContext(context);
+        for (String permission : permissions) {
+            if (check(context, permission)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
