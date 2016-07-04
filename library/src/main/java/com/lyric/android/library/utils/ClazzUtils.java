b6f379326b5ea07f84bd6f4455a7dd81d46cab83
lyricgan <lyricgan88@163.com>
2016-07-04 17:35:01 +0800

    update

--- library/src/main/java/com/lyric/android/library/utils/ClazzUtils.java
+++ library/src/main/java/com/lyric/android/library/utils/ClazzUtils.java
@@ -1,10 +1,16 @@
 package com.lyric.android.library.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * @author lyric
@@ -91,5 +97,74 @@ public class ClazzUtils {
             }
         }
         return type;
+    }
+
+    /**
+     * 查找接口下的所有实现类
+     * @param clazz 接口
+     * @return 接口下的所有实现类
+     */
+    public static List<Class> getAllClassByInterface(Class clazz) {
+        List<Class> returnClassList = new ArrayList<Class>();
+        // 判断是否为一个接口
+        if (clazz.isInterface()) {
+            String packageName = clazz.getPackage().getName();
+            try {
+                List<Class> allClassList = getClasses(packageName); //获得当前包下以及子包下的所有类
+                //判断是否是同一个接口
+                for (int i = 0; i < allClassList.size(); i++) {
+                    if (clazz.isAssignableFrom(allClassList.get(i))) { //判断是不是一个接口
+                        if (!clazz.equals(allClassList.get(i))) { //本身不加进去
+                            returnClassList.add(allClassList.get(i));
+                        }
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return returnClassList;
+    }
+
+    /**
+     * 从一个包中查找出所有的类，在jar包中不能查找
+     * @param packageName 包名
+     * @return 当前包下以及子包下的所有类
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+    private static List<Class> getClasses(String packageName) throws ClassNotFoundException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<File>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        ArrayList<Class> classes = new ArrayList<Class>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        return classes;
+    }
+
+    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<Class>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                assert !file.getName().contains(".");
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
     }
 }
