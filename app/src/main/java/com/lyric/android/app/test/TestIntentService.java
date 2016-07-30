9d6629c1b66f2d7e9b6b749d86d2ad9dc86d46d1
lyricgan <lyricgan88@163.com>
2016-07-30 22:04:44 +0800

    update

---
+++ app/src/main/java/com/lyric/android/app/test/TestIntentService.java
@@ -0,0 +1,24 @@
+package com.lyric.android.app.test;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * @author lyric
+ * @description
+ * @time 16/7/30
+ */
+public class TestIntentService extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public TestIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+    }
+}
