8f078a27cc08f023e7abc781729025967fbff866
lyricgan <lyricgan88@163.com>
2016-09-05 18:12:31 +0800

    update

--- library/src/main/java/com/lyric/android/library/utils/ActivityUtils.java
+++ library/src/main/java/com/lyric/android/library/utils/ActivityUtils.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 
 /**
  * @author ganyu
@@ -45,10 +44,10 @@ public class ActivityUtils {
             }
             context.startActivity(intent);
         } else {
-            if (context instanceof FragmentActivity) {
-                ((FragmentActivity) context).startActivityForResult(intent, requestCode);
-            } else if (context instanceof Activity) {
+            if (context instanceof Activity) {
                 ((Activity) context).startActivityForResult(intent, requestCode);
+            } else {
+                throw new IllegalArgumentException("Context must be instance of Activity");
             }
         }
     }
