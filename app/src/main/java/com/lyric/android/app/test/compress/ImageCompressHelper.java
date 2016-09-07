488f9ba2d0774b50f621a97a2fc8f6e06d80cede
lyricgan <lyricgan88@163.com>
2016-09-07 20:06:48 +0800

    update

--- app/src/main/java/com/lyric/android/app/test/compress/ImageCompressHelper.java
+++ app/src/main/java/com/lyric/android/app/test/compress/ImageCompressHelper.java
@@ -86,11 +86,10 @@ public class ImageCompressHelper {
      */
     public String execute(String srcImageUri, int outWidth, int outHeight, int maxFileSize, String outFilePath) {
         String srcImagePath = getFilePathFromUri(srcImageUri);
-        //进行大小缩放来达到压缩的目的
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(srcImagePath, options);
-        //根据原始图片的宽高比和期望的输出图片的宽高比计算最终输出的图片的宽和高
+        // 根据原始图片的宽高比和期望的输出图片的宽高比计算最终输出的图片的宽和高
         float srcWidth = options.outWidth;
         float srcHeight = options.outHeight;
         float maxWidth = outWidth;
@@ -99,14 +98,14 @@ public class ImageCompressHelper {
         float outRatio = maxWidth / maxHeight;
         float actualOutWidth = srcWidth;
         float actualOutHeight = srcHeight;
+        /**
+         * 计算规则：
+         * 如果输入比率小于输出比率，则最终输出的宽度以maxHeight为准
+         * 例如输入比为10:20，输出比是300:10，如果要保证输出图片的宽高比和原始图片的宽高比相同，则最终输出图片的高为10，宽度为(10/20) * 10 = 5，最终输出图片的比率为5:10，和原始输入的比率相同。
+         * 如果输入比率大于输出比率，则最终输出的高度以maxHeight为准
+         * 例如输入比为20:10，输出比是5:100，如果要保证输出图片的宽高比和原始图片的宽高比相同，则最终输出图片的宽为5，高度需要根据输入图片的比率计算获得为5/(20/10)= 2.5，最终输出图片的比率为5:2.5 和原始输入的比率相同
+         */
         if (srcWidth > maxWidth || srcHeight > maxHeight) {
-            //如果输入比率小于输出比率,则最终输出的宽度以maxHeight为准()
-            //比如输入比为10:20 输出比是300:10 如果要保证输出图片的宽高比和原始图片的宽高比相同,则最终输出图片的高为10
-            //宽度为10/20 * 10 = 5  最终输出图片的比率为5:10 和原始输入的比率相同
-
-            //同理如果输入比率大于输出比率,则最终输出的高度以maxHeight为准()
-            //比如输入比为20:10 输出比是5:100 如果要保证输出图片的宽高比和原始图片的宽高比相同,则最终输出图片的宽为5
-            //高度需要根据输入图片的比率计算获得 为5 / 20/10= 2.5  最终输出图片的比率为5:2.5 和原始输入的比率相同
             if (srcRatio < outRatio) {
                 actualOutHeight = maxHeight;
                 actualOutWidth = actualOutHeight * srcRatio;
@@ -126,8 +125,9 @@ public class ImageCompressHelper {
         } catch (OutOfMemoryError e) {
             e.printStackTrace();
         }
+        // 压缩失败则返回空
         if (scaledBitmap == null) {
-            return null;// 压缩失败
+            return null;
         }
         // 生成最终输出的bitmap
         Bitmap actualOutBitmap = Bitmap.createScaledBitmap(scaledBitmap, (int) actualOutWidth, (int) actualOutHeight, true);
@@ -205,8 +205,8 @@ public class ImageCompressHelper {
     }
 
     private int computeSampleSize(BitmapFactory.Options options, float reqWidth, float reqHeight) {
-        float srcWidth = options.outWidth;//20
-        float srcHeight = options.outHeight;//10
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
         int sampleSize = 1;
         if (srcWidth > reqWidth || srcHeight > reqHeight) {
             int withRatio = Math.round(srcWidth / reqWidth);
