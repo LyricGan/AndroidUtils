1abe399512883dfd6f253b407fa21f2bb8bf3104
lyricgan <lyricgan88@163.com>
2017-12-02 19:44:13 +0800

    ContentProvider调整

--- app/src/main/java/com/lyric/android/app/test/TestContentProvider.java
+++ app/src/main/java/com/lyric/android/app/test/TestContentProvider.java
@@ -10,6 +10,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.provider.BaseColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
@@ -17,6 +18,7 @@ import android.text.TextUtils;
 import java.util.HashMap;
 
 /**
+ * ContentProvider
  * @author lyricgan
  * @time 2016/8/3 16:49
  */
@@ -26,18 +28,20 @@ public class TestContentProvider extends ContentProvider {
     private static final int INDICATOR_ITEM = 2;
     private static final String PATH = "path_test";
     private static HashMap<String, String> mProjectionMap;
+
+    public static final String AUTHORITY = "com.provider.test";
     private DatabaseHelper mDatabaseHelper;
 
     static {
         mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-        mUriMatcher.addURI(TestDatabase.AUTHORITY, PATH, INDICATOR_GROUP);
-        mUriMatcher.addURI(TestDatabase.AUTHORITY, PATH + "/#", INDICATOR_ITEM);
+        mUriMatcher.addURI(AUTHORITY, PATH, INDICATOR_GROUP);
+        mUriMatcher.addURI(AUTHORITY, PATH + "/#", INDICATOR_ITEM);
 
         mProjectionMap = new HashMap<>();
-        mProjectionMap.put(TestDatabase.ItemColumns._ID, TestDatabase.ItemColumns._ID);
-        mProjectionMap.put(TestDatabase.ItemColumns.TITLE, TestDatabase.ItemColumns.TITLE);
-        mProjectionMap.put(TestDatabase.ItemColumns.CONTENT, TestDatabase.ItemColumns.CONTENT);
-        mProjectionMap.put(TestDatabase.ItemColumns.CREATE_DATE, TestDatabase.ItemColumns.CREATE_DATE);
+        mProjectionMap.put(ItemColumns._ID, ItemColumns._ID);
+        mProjectionMap.put(ItemColumns.TITLE, ItemColumns.TITLE);
+        mProjectionMap.put(ItemColumns.CONTENT, ItemColumns.CONTENT);
+        mProjectionMap.put(ItemColumns.CREATE_DATE, ItemColumns.CREATE_DATE);
     }
 
     @Override
@@ -53,15 +57,15 @@ public class TestContentProvider extends ContentProvider {
         switch(mUriMatcher.match(uri)) {
             case INDICATOR_GROUP: {
                 // 设置查询的表
-                queryBuilder.setTables(TestDatabase.ItemColumns.TABLE_NAME);
+                queryBuilder.setTables(ItemColumns.TABLE_NAME);
                 // 设置投影映射
                 queryBuilder.setProjectionMap(mProjectionMap);
             }
                 break;
             case INDICATOR_ITEM: {
-                queryBuilder.setTables(TestDatabase.ItemColumns.TABLE_NAME);
+                queryBuilder.setTables(ItemColumns.TABLE_NAME);
                 queryBuilder.setProjectionMap(mProjectionMap);
-                queryBuilder.appendWhere(TestDatabase.ItemColumns._ID + "=" + uri.getPathSegments().get(1));
+                queryBuilder.appendWhere(ItemColumns._ID + "=" + uri.getPathSegments().get(1));
             }
                 break;
             default:
@@ -69,7 +73,7 @@ public class TestContentProvider extends ContentProvider {
         }
         String orderBy;
         if (TextUtils.isEmpty(sortOrder)) {
-            orderBy = TestDatabase.ItemColumns.DEFAULT_ORDER_BY;
+            orderBy = ItemColumns.DEFAULT_ORDER_BY;
         } else {
             orderBy = sortOrder;
         }
@@ -82,10 +86,10 @@ public class TestContentProvider extends ContentProvider {
     public String getType(@NonNull Uri uri) {
         switch (mUriMatcher.match(uri)) {
             case INDICATOR_GROUP: {
-                return TestDatabase.ItemColumns.CONTENT_TYPE;
+                return ItemColumns.CONTENT_TYPE;
             }
             case INDICATOR_ITEM: {
-                return TestDatabase.ItemColumns.CONTENT_ITEM_TYPE;
+                return ItemColumns.CONTENT_ITEM_TYPE;
             }
         }
         return null;
@@ -98,9 +102,9 @@ public class TestContentProvider extends ContentProvider {
             throw new IllegalArgumentException("Unknown URI " + uri);
         }
         SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
-        long rowID = db.insert(TestDatabase.ItemColumns.TABLE_NAME, null, values);
+        long rowID = db.insert(ItemColumns.TABLE_NAME, null, values);
         if (rowID > 0) {
-            return ContentUris.withAppendedId(TestDatabase.ItemColumns.CONTENT_URI, rowID);
+            return ContentUris.withAppendedId(ItemColumns.CONTENT_URI, rowID);
         }
         return null;
     }
@@ -111,12 +115,12 @@ public class TestContentProvider extends ContentProvider {
         int count;
         switch (mUriMatcher.match(uri)) {
             case INDICATOR_GROUP: {
-                count = db.delete(TestDatabase.ItemColumns.TABLE_NAME, selection, selectionArgs);
+                count = db.delete(ItemColumns.TABLE_NAME, selection, selectionArgs);
             }
                 break;
             case INDICATOR_ITEM: {
                 String rowId = uri.getPathSegments().get(1);
-                count = db.delete(TestDatabase.ItemColumns.TABLE_NAME, TestDatabase.ItemColumns._ID + "=" + rowId, null);
+                count = db.delete(ItemColumns.TABLE_NAME, ItemColumns._ID + "=" + rowId, null);
             }
                 break;
             default:
@@ -135,12 +139,12 @@ public class TestContentProvider extends ContentProvider {
         int count;
         switch(mUriMatcher.match(uri)) {
             case INDICATOR_GROUP: {
-                count = db.update(TestDatabase.ItemColumns.TABLE_NAME, values, null, null);
+                count = db.update(ItemColumns.TABLE_NAME, values, null, null);
             }
                 break;
             case INDICATOR_ITEM: {
                 String rowId = uri.getPathSegments().get(1);
-                count = db.update(TestDatabase.ItemColumns.TABLE_NAME, values, TestDatabase.ItemColumns._ID + "=" + rowId, null);
+                count = db.update(ItemColumns.TABLE_NAME, values, ItemColumns._ID + "=" + rowId, null);
             }
                 break;
             default: {
@@ -153,20 +157,42 @@ public class TestContentProvider extends ContentProvider {
         return count;
     }
 
+    public static final class ItemColumns implements BaseColumns {
+        public static final String TABLE_NAME = "test";
+        public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE_NAME);
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + TABLE_NAME;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + TABLE_NAME;
+
+        public static final String TITLE = "title";
+        public static final String CONTENT = "content";
+        public static final String CREATE_DATE = "create_date";
+
+        public static final String DEFAULT_ORDER_BY = "create_date DESC";
+
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ("
+                + _ID + " INTEGER PRIMARY KEY,"
+                + TITLE + " VARCHAR(50),"
+                + CONTENT + " TEXT,"
+                + CREATE_DATE + " INTEGER"
+                + ");" ;
+    }
+
     private static class DatabaseHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "test.db";
+        private static final int DATABASE_VERSION = 1;
 
         public DatabaseHelper(Context context) {
-            super(context, TestDatabase.DATABASE_NAME, null, TestDatabase.DATABASE_VERSION);
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL(TestDatabase.ItemColumns.SQL_CREATE_TABLE);
+            db.execSQL(ItemColumns.SQL_CREATE_TABLE);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + TestDatabase.ItemColumns.TABLE_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + ItemColumns.TABLE_NAME);
             onCreate(db);
         }
     }
