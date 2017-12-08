dcbd371be79993c86dd0d49f97379f4597b48de0
lyricgan <lyricgan88@163.com>
2017-12-08 11:21:21 +0800

    异常处理工具类

--- app/src/main/java/com/lyric/android/app/utils/ExceptionHandler.java
+++ app/src/main/java/com/lyric/android/app/utils/ExceptionHandler.java
@@ -5,10 +5,12 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.text.TextUtils;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -22,35 +24,36 @@ import java.util.TreeSet;
  * @author lyricgan
  * @time 2016/8/30 17:29
  */
-public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+public abstract class ExceptionHandler implements Thread.UncaughtExceptionHandler {
     private static final String TAG = ExceptionHandler.class.getSimpleName();
-    /** 异常捕获处理处理类 */
-    private Thread.UncaughtExceptionHandler mHandler;
-    private Context mContext;
-
-    // 使用Properties来保存设备的信息和错误堆栈信息
-    private Properties mCrashProperties = new Properties();
+    /** 错误报告文件的扩展名 */
+    private static final String CRASH_REPORTER_EXTENSION = ".cr";
     private static final String VERSION_NAME = "version_name";
     private static final String VERSION_CODE = "version_code";
     private static final String STACK_TRACE = "stack_trace";
-    /** 错误报告文件的扩展名 */
-    private static final String CRASH_REPORTER_EXTENSION = ".cr";
 
-    public ExceptionHandler(Context context) {
-        this(context, null);
-    }
+    private Context mContext;
+    /** 异常捕获处理对象 */
+    private Thread.UncaughtExceptionHandler mHandler;
+
+    // 使用Properties来保存设备的信息和错误堆栈信息
+    private Properties mCrashProperties = new Properties();
 
     public ExceptionHandler(Context context, Thread.UncaughtExceptionHandler handler) {
-        mContext = context.getApplicationContext();
         if (handler == null) {
-            mHandler = Thread.getDefaultUncaughtExceptionHandler();
+            handler = Thread.getDefaultUncaughtExceptionHandler();
         }
+        this.mContext = context.getApplicationContext();
+        this.mHandler = handler;
         Thread.setDefaultUncaughtExceptionHandler(this);
     }
 
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
-        if (ex != null && handleException(ex)) {
+        if (ex == null) {
+            return;
+        }
+        if (handleException(ex, mContext)) {
             ex.printStackTrace();
         } else {
             mHandler.uncaughtException(thread, ex);
@@ -60,35 +63,29 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
     /**
      * 处理异常，收集错误信息并发送错误报告
      * @param ex 异常信息
-     * @return true:如果处理了该异常信息;否则返回false
+     * @param context 上下文
+     * @return true or false
      */
-    private boolean handleException(Throwable ex) {
-        if (ex == null) {
+    private boolean handleException(Throwable ex, Context context) {
+        String localizedMessage = ex.getLocalizedMessage();
+        String stackTraceMessages = getStackTraceMessage();
+        Log.w(TAG, "localizedMessage:" + localizedMessage + ",stackTraceMessages:" + stackTraceMessages);
+
+        collectCrashDeviceInfo(context);
+        // 保存错误报告文件
+        String fileName = saveCrashFile(context, ex);
+        if (!TextUtils.isEmpty(fileName)) {
+            sendCrashReportFiles(context);
             return true;
         }
-        String msg = ex.getLocalizedMessage();
-        String name = getStackTraceMessage();
-        // 打印异常信息
-        if (TextUtils.isEmpty(name)) {
-            LogUtils.e(TAG, "handleException:" + msg);
-        } else {
-            LogUtils.e(TAG, "handleException:" + name + "-" + msg);
-        }
-        // 收集设备信息
-        collectCrashDeviceInfo(mContext);
-        // 保存错误报告文件
-        saveCrashInfoToFile(ex);
-        // 发送错误报告到服务器
-        sendCrashReportFiles(mContext);
-
-        return true;
+        return false;
     }
 
     /**
      * 获取堆栈信息
      * @return 获取异常日志堆栈信息
      */
-    public String getStackTraceMessage() {
+    private String getStackTraceMessage() {
         StackTraceElement[] stackTraceElementArray = Thread.currentThread().getStackTrace();
         if (stackTraceElementArray == null) {
             return null;
@@ -112,82 +109,10 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
     }
 
     /**
-     * 上传错误报告文件
-     * @param context 应用上下文
-     */
-    public void sendCrashReportFiles(Context context) {
-        String[] crFiles = getCrashReportFiles(context);
-        if (crFiles != null && crFiles.length > 0) {
-            TreeSet<String> sortedFiles = new TreeSet<>();
-            sortedFiles.addAll(Arrays.asList(crFiles));
-            for (String fileName : sortedFiles) {
-                File crFile = new File(context.getFilesDir(), fileName);
-                sendCrashReportFile(crFile);
-            }
-        }
-    }
-
-    /**
-     * 发送错误报告到服务器
-     * @param file 错误收集文件
-     */
-    private void sendCrashReportFile(File file) {
-    }
-
-    /**
-     * 获取错误报告文件名
-     * @param context 应用上下文
-     * @return 异常文件目录
-     */
-    private String[] getCrashReportFiles(Context context) {
-        File filesDir = context.getFilesDir();
-        FilenameFilter filter = new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(CRASH_REPORTER_EXTENSION);
-            }
-        };
-        return filesDir.list(filter);
-    }
-
-    /**
-     * 保存错误信息到文件中
-     * @param ex 异常信息
-     * @return 异常收集文件名称
-     */
-    private String saveCrashInfoToFile(Throwable ex) {
-        Writer info = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(info);
-        ex.printStackTrace(printWriter);
-
-        Throwable cause = ex.getCause();
-        while (cause != null) {
-            cause.printStackTrace(printWriter);
-            cause = cause.getCause();
-        }
-        String result = info.toString();
-        printWriter.close();
-        mCrashProperties.put(STACK_TRACE, result);
-        try {
-            long timestamp = System.currentTimeMillis();
-            String fileName = "crash-" + timestamp + CRASH_REPORTER_EXTENSION;
-            FileOutputStream trace = mContext.openFileOutput(fileName, Context.MODE_PRIVATE);
-            mCrashProperties.store(trace, "trace");
-            trace.flush();
-            trace.close();
-
-            return fileName;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
      * 收集程序崩溃的设备信息
      * @param context 应用上下文
      */
-    public void collectCrashDeviceInfo(Context context) {
+    private void collectCrashDeviceInfo(Context context) {
         try {
             PackageManager packageManager = context.getPackageManager();
             PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES);
@@ -209,4 +134,84 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
             }
         }
     }
+
+    /**
+     * 保存错误信息到文件中
+     * @param context 上下文
+     * @param ex 异常信息
+     * @return 异常收集文件名称
+     */
+    private String saveCrashFile(Context context, Throwable ex) {
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        ex.printStackTrace(printWriter);
+
+        Throwable cause = ex.getCause();
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        String result = writer.toString();
+        printWriter.close();
+        mCrashProperties.put(STACK_TRACE, result);
+        // 设置文件名称
+        String fileName = "crash-" + System.currentTimeMillis() + CRASH_REPORTER_EXTENSION;
+        FileOutputStream traceStream = null;
+        try {
+            traceStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            mCrashProperties.store(traceStream, "trace");
+            traceStream.flush();
+
+            return fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (traceStream != null) {
+                try {
+                    traceStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取错误报告文件名
+     * @param context 应用上下文
+     * @return 异常文件目录
+     */
+    private String[] getCrashReportFiles(Context context) {
+        File filesDir = context.getFilesDir();
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(CRASH_REPORTER_EXTENSION);
+            }
+        };
+        return filesDir.list(filter);
+    }
+
+    /**
+     * 上传错误报告文件
+     * @param context 应用上下文
+     */
+    private void sendCrashReportFiles(Context context) {
+        String[] crashReportFiles = getCrashReportFiles(context);
+        if (crashReportFiles != null && crashReportFiles.length > 0) {
+            TreeSet<String> sortedFiles = new TreeSet<>();
+            sortedFiles.addAll(Arrays.asList(crashReportFiles));
+            for (String fileName : sortedFiles) {
+                File crashFile = new File(context.getFilesDir(), fileName);
+                sendCrashReportFile(crashFile);
+            }
+        }
+    }
+
+    /**
+     * 发送错误报告文件
+     * @param file 错误收集文件
+     */
+    public abstract void sendCrashReportFile(File file);
 }
