8b5da46423dfdeddd5abd219031f67ed95db06d3
lyricgan <lyricgan88@163.com>
2018-07-13 18:16:45 +0800

    update test

--- app/src/main/java/com/lyric/android/app/widget/MovedCircleView.java
+++ app/src/main/java/com/lyric/android/app/widget/MovedCircleView.java
@@ -12,18 +12,20 @@ import com.lyric.android.app.utils.DisplayUtils;
 
 /**
  * @author lyricgan
- * @time 2016/3/15 15:07
  */
 public class MovedCircleView extends View {
-    private static final String TAG = MovedCircleView.class.getSimpleName();
     private static final int DEFAULT_COLOR = 0xff007eff;
     private static final int PRESSED_COLOR = 0x88007eff;
-    private TextPaint mPaint = new TextPaint();
+
+    private int mDefaultWidth;
+    private int mDefaultHeight;
+    private int mMinDistance;
+    private TextPaint mPaint;
+
     private float mLastX;
     private float mLastY;
     private float mCurrentX;
     private float mCurrentY;
-    private int mMinDistance;
 
     public MovedCircleView(Context context) {
         this(context, null);
@@ -35,11 +37,16 @@ public class MovedCircleView extends View {
 
     public MovedCircleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initialize(context);
+        init(context);
     }
 
-    private void initialize(Context context) {
+    private void init(Context context) {
+        mDefaultWidth = DisplayUtils.dip2px(getContext(), 64);
+        mDefaultHeight = DisplayUtils.dip2px(getContext(), 64);
         mMinDistance = DisplayUtils.dip2px(context, 16);
+
+        mPaint = new TextPaint();
+        mPaint.setColor(DEFAULT_COLOR);
 
         invalidate();
     }
@@ -53,61 +60,53 @@ public class MovedCircleView extends View {
         int heightMeasureSpecSize = MeasureSpec.getSize(heightMeasureSpec);
         // 处理wrap_content的情况
         if (widthMeasureSpecMode == MeasureSpec.AT_MOST && heightMeasureSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(getDefaultWidth(), getDefaultHeight());
+            setMeasuredDimension(mDefaultWidth, mDefaultHeight);
         } else if (widthMeasureSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(getDefaultWidth(), heightMeasureSpecSize);
+            setMeasuredDimension(mDefaultWidth, heightMeasureSpecSize);
         } else if (heightMeasureSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(widthMeasureSpecSize, getDefaultHeight());
+            setMeasuredDimension(widthMeasureSpecSize, mDefaultHeight);
         }
-    }
-
-    private int getDefaultWidth() {
-        return DisplayUtils.dip2px(getContext(), 64);
-    }
-
-    private int getDefaultHeight() {
-        return DisplayUtils.dip2px(getContext(), 64);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         int radius = getWidth() / 2;
-        if (mCurrentX == 0 || mCurrentY == 0) {
-            mPaint.setColor(DEFAULT_COLOR);
-            canvas.drawCircle(radius, radius, radius, mPaint);
-        } else {
-            canvas.drawCircle(mCurrentX, mCurrentY, radius, mPaint);
+        float cx = mCurrentX;
+        float cy = mCurrentY;
+        if (cx == 0 || cy == 0) {
+            cx = cy = radius;
         }
+        canvas.drawCircle(cx, cy, radius, mPaint);
         canvas.restore();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 ViewParent viewParent = getParent();
                 if (viewParent != null) {
                     viewParent.requestDisallowInterceptTouchEvent(true);
                 }
-                mLastX = x;
-                mLastY = y;
+                mLastX = event.getX();
+                mLastY = event.getY();
                 mPaint.setColor(PRESSED_COLOR);
                 invalidate();
                 return true;
             case MotionEvent.ACTION_MOVE:
-                mCurrentX = x;
-                mCurrentY = y;
+                mCurrentX = event.getX();
+                mCurrentY = event.getY();
                 if (Math.abs(mCurrentX - mLastX) > mMinDistance || Math.abs(mCurrentY - mLastY) > mMinDistance) {
                     invalidate();
-                    return true;
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 mPaint.setColor(DEFAULT_COLOR);
                 invalidate();
+                if (Math.abs(event.getX() - mLastX) <= 3 && Math.abs(event.getY() - mLastY) <= 3) {
+                    return performClick();
+                }
                 break;
         }
         return super.onTouchEvent(event);
